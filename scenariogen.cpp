132c5c16a9df39f5aa21d402e895167c2f370f32
jpbarona <juanpabarvel@gmail.com>
2025-03-02 21:57:23 +0000

    Single vector implementation

--- scenariogen.cpp
+++ scenariogen.cpp
@@ -8,16 +8,13 @@ using namespace std;
 #define DEFAULT_SEED 69
 #define DT 252.0
 
+
 struct ScenarioMatrix{
-    vector<vector<double>> scenarios;
-    int num_steps;
-    int num_scenarios;
+    vector<double> scenarios;
+    const int num_steps;
+    const int num_scenarios;
     ScenarioMatrix(int num_steps, int num_scenarios) : num_steps(num_steps), num_scenarios(num_scenarios) {
-        scenarios.resize(num_scenarios+1);
-        for (int i = 0; i < num_scenarios; i++)
-        {
-            scenarios[i].resize(num_steps);
-        }
+        scenarios.resize(num_scenarios*num_steps);
     }
 };
 
@@ -35,18 +32,19 @@ struct Equity : public Macro {
     Equity(double p, double v, const Macro &m) : Macro(m), price(p), volatility(v) {}
 };
 
-void save_as_csv(vector<vector<double>> scenarios, string filename) {
+void save_as_csv(ScenarioMatrix scenario_matrix, string filename) {
     ofstream file(filename);
-    int num_scenarios = scenarios.size();
-    int num_days = scenarios[0].size();
+    int num_scenarios = scenario_matrix.num_scenarios;
+    int num_steps = scenario_matrix.num_steps;
+    vector<double>& scenarios = scenario_matrix.scenarios;
 
     for (int scenario = 0; scenario < num_scenarios; scenario++)
     {
-    vector<double>& scenario_vector = scenarios[scenario];
-        for (int day = 0; day < num_days; day++)
+        file << scenario << ",";
+        for (int time = 0; time < num_steps; time++)
         {
-            file << scenario_vector[day];
-            if (day != num_days-1) {
+            file << scenarios[scenario*num_steps + time];
+            if (time != num_steps-1) {
                 file << ",";
             }
         }
@@ -70,42 +68,44 @@ class EquityPriceGenerator {
     EquityPriceGenerator(Equity e, Macro m) : rRate(m.riskfreerate), volatility(e.volatility), dt(m.dt), initial_price(e.price), mtEngine(m.seed) {}
     
 
-    vector<vector<double>> generateScenarios(int num_steps, int num_scenarios) {
+    ScenarioMatrix generateScenarios(int num_steps, int num_scenarios) {
         ScenarioMatrix scenario_matrix = ScenarioMatrix(num_steps, num_scenarios);
         set_initial_price(scenario_matrix, this->initial_price);
         for (int time = 1; time < num_steps; time++)
         {
             generate_scenario_vector(scenario_matrix, time);
         }
-        return scenario_matrix.scenarios;
+        return scenario_matrix;
     }
     
     private:
     const double expArg1 = (rRate - (volatility * volatility / 2.0)) * dt;
     double nextPrice(double current_price, double norm) {
-
         double expArg2 = volatility * sqrt(dt) * norm;
         double newprice = current_price * exp(expArg1 + expArg2);
         return newprice;
     }
 
     void generate_scenario_vector(ScenarioMatrix &scenario_matrix, int time) {
-        vector<vector<double>>& scenarios = scenario_matrix.scenarios;
+        vector<double>& scenarios = scenario_matrix.scenarios;
+        
         int num_scenarios = scenario_matrix.num_scenarios;
+        int num_steps = scenario_matrix.num_steps;
         for (int scenario = 0; scenario < num_scenarios; scenario++)
         {
             double norm = nd(mtEngine);
-            double current_price = scenarios[scenario][time-1];
+            double current_price = scenarios[scenario*num_steps + time-1];
             double next_price = nextPrice(current_price, norm);
-            scenarios[scenario][time] = next_price;
+            scenarios[scenario*num_steps + time] = next_price;
         }
     }
     void set_initial_price(ScenarioMatrix &scenario_matrix, double price) {
         int num_scenarios = scenario_matrix.num_scenarios;
-        vector<vector<double>>& scenarios = scenario_matrix.scenarios;
+        int num_steps = scenario_matrix.num_steps;
+        vector<double>& scenarios = scenario_matrix.scenarios;
         for (int scenario = 0; scenario < num_scenarios; scenario++)
         {
-            scenarios[scenario][0] = price;
+            scenarios[scenario*num_steps] = price;
         }   
     }
 
